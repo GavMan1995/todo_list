e0771674a710be82b353c10de193c63fc883a0ca
GavMan1995 <gavyncaldwell@gmail.com>
2016-03-01 17:43:14 -0700

    finished somewhat the too list

--- todo_list/db/migrate/20160301230715_create_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateLists < ActiveRecord::Migration
-  def change
-    create_table :lists do |t|
-      t.string :item
-
-      t.timestamps null: false
-    end
-  end
-end
--- todo_list/db/schema.rb
+++ todo_list/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301230715) do
+ActiveRecord::Schema.define(version: 20160301233938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string   "item"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "item"
+    t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
